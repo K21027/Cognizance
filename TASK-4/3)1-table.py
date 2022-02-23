8cc049a2f3d5b25b0acc55a63fcd24d489994950
K21027 <99636212+K21027@users.noreply.github.com>
2022-02-23 20:50:15 +0530

    Add files via upload

---
+++ TASK-4/3)1-table.py
@@ -0,0 +1,12 @@
+lst = []
+n = int(input("Enter number of rows : "))
+print("Enter the roll number , name and marks respectively to output a data table")
+for i in range(0, n):
+    ele = [input(), input(),input()]
+    lst.append(ele)
+print("Enter the header for the created table")
+print("{:<10} {:<10} {:<10} ".format(input(), input(),input()))
+for a in lst:
+    for b in a:
+        print(b,end=" "*10)
+    print()
