fba73bb3aae47e60a1743020ec25c69079e0e359
K21027 <99636212+K21027@users.noreply.github.com>
2022-03-09 16:50:49 +0530

    Delete 4-uppercase.py

--- TASK-8/4-uppercase.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-series = pd.Series(['amrita', 'school', 'of', 'engineering' 'chennai' , 'campus'])
-newseries = series.str.title()
-print("resulting series : ")
-print(newseries)
-
