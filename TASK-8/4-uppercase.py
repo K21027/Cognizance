2b455357929d317d7378c55f6742e472505e0891
K21027 <99636212+K21027@users.noreply.github.com>
2022-03-09 16:46:06 +0530

    Add files via upload

---
+++ TASK-8/4-uppercase.py
@@ -0,0 +1,6 @@
+import pandas as pd
+series = pd.Series(['amrita', 'school', 'of', 'engineering' 'chennai' , 'campus'])
+newseries = series.str.title()
+print("resulting series : ")
+print(newseries)
+
